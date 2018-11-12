5b0eee48cb7c1c710ac31a98806601301f9f663f
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-11-12 17:37:25 +0900

    web view try

--- BookFinder/MainTableViewController.swift
+++ BookFinder/MainTableViewController.swift
@@ -79,6 +79,7 @@ class MainTableViewController: UITableViewController {
     }
 
     
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         if let items1 = items {
@@ -97,7 +98,7 @@ class MainTableViewController: UITableViewController {
         return cell
     }
     
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
--- BookFinder/ViewController.swift
+++ BookFinder/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myWebView: UIWebView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadWebPage("https:book.naver.com/bookdb/book_detail.nhn?bid=6103765")
+       
     }
 
+    func loadWebPage(_ url: String) {
+        let myUrl = URL(string: url)
+        let myRequest = URLRequest(url: myUrl!)
+        myWebView.loadRequest(myRequest)
+        
+    }
+    
+   
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
